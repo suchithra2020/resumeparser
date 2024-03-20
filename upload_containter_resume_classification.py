efcc506e54d4ed2f54106e7a959e8a3c5e1b9ef4
suchithra2020 <162819429+suchithra2020@users.noreply.github.com>
2024-03-21 00:25:05 +0530

    Update upload_containter_resume_classification.py

--- upload_containter_resume_classification.py
+++ upload_containter_resume_classification.py
@@ -117,14 +117,6 @@ if menu_id == 'Home':
 
 \
 
-'**♡ Thank you for taking the time ♡**', unsafe_allow_html=True)
-
-            st.markdown('<img align="right" alt="code"  height="200" width="200" src = "https://static.wixstatic.com/media/15e6c3_8f8cac375de143dc9d1d552090d975cf~mv2.gif">', unsafe_allow_html=True)
-
-        with col2:
-
-            st.markdown('<img align="right" alt="code"  height="500" width="300" src = "https://github.com/MoinDalvs/Resume_Parser_and_Classification/blob/main/Images/Scroll_down.gif?raw=true">', unsafe_allow_html=True)
-
 def extract_text_from_docx(docx_path):
 
     if docx_path.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
